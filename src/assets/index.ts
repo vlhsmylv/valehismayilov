0c835ebf93b872f841eb5ffea47125b41075f3ce
vlhsmylv <iam.valeh.ismayilov@gmail.com>
2023-09-03 22:33:28 +0400

    Adding images to local directory

---
+++ src/assets/index.ts
@@ -0,0 +1,32 @@
+import bitlio from "./images/bitlio.jpeg";
+import hoobank from "./images/hoobank.png";
+import lightbox from "./images/lightbox.jpeg";
+import linkfold from "./images/linkfold.png";
+import notificationsPage from "./images/notifications-page.png";
+import rentARide from "./images/rent-a-ride.jpeg";
+import telmanHeybat from "./images/telman-heybat.jpeg";
+import threads from "./images/threads.png";
+
+export {
+  bitlio,
+  hoobank,
+  lightbox,
+  linkfold,
+  notificationsPage,
+  rentARide,
+  telmanHeybat,
+  threads,
+};
+
+export const projectImages: {
+  [key: string]: string;
+} = {
+  bitlio,
+  hoobank,
+  lightbox,
+  linkfold,
+  notificationsPage,
+  rentARide,
+  telmanHeybat,
+  threads,
+};
