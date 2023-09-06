32b8507174e8feb0022e5e29fd15014b803d519d
vlhsmylv <iam.valeh.ismayilov@gmail.com>
2023-09-06 18:33:05 +0400

    Image optimization and favicon

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,13 +1,13 @@
-import bitlio from "./images/bitlio.jpeg";
-import hoobank from "./images/hoobank.jpeg";
-import lightbox from "./images/lightbox.jpeg";
-import linkfold from "./images/linkfold.jpeg";
-import notificationsPage from "./images/notifications-page.jpeg";
-import rentARide from "./images/rent-a-ride.jpeg";
-import telmanHeybat from "./images/telman-heybat.jpeg";
-import threads from "./images/threads.jpeg";
-import newsletterSignUp from "./images/newsletter-sign-up.jpeg";
-import me from "./images/me.png";
+import bitlio from "./images/bitlio.webp";
+import hoobank from "./images/hoobank.webp";
+import lightbox from "./images/lightbox.webp";
+import linkfold from "./images/linkfold.webp";
+import notificationsPage from "./images/notifications-page.webp";
+import rentARide from "./images/rent-a-ride.webp";
+import telmanHeybat from "./images/telman-heybat.webp";
+import threads from "./images/threads.webp";
+import newsletterSignUp from "./images/newsletter-sign-up.webp";
+import me from "./images/me.webp";
 
 export {
   bitlio,
