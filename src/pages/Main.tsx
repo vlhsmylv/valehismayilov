3e36a1c47020c9db90744d538f8ded8969939270
vlhsmylv <iam.valeh.ismayilov@gmail.com>
2023-09-09 16:56:25 +0400

    Adding Mixpanel

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,6 +5,15 @@ import Hero from "../components/Hero";
 import MySkills from "../components/MySkills";
 import Portfolio from "../components/Portfolio";
 
+// Mixpanel import
+import mixpanel from "mixpanel-browser";
+
+mixpanel.init("c0c8c3fe996b5c84bfbf6bc0b23b9177", {
+  debug: true,
+  track_pageview: true,
+  persistence: "localStorage",
+});
+
 const Main = () => {
   return (
     <>
